54b793b60fe04e268fdbd5953a7648cda53872fc
Leonardo Muros do Nascimento <lmuros@gmail.com>
2021-08-26 10:26:07 -0300

    Criação da página 2 com os detalhes dos países

--- src/components/DetalhesPais/index.js
+++ src/components/DetalhesPais/index.js
@@ -1,10 +1,122 @@
 import { Header } from "../../Layout/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-export const DetalhesPais = () => {
+export const DetalhesPais = (props) => {
+  const error = "Informações não encontradas..."
+  const nome = props.match.params.name;
+  const [atualizaNome, setAtualizaNome] = useState(nome);
+  const [paisesDetalhes, setPaisDetalhes] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [vizinhos, setVizinhos] = useState(null);
+  const [vizinhosFinal, setVizinhosFinal] = useState([]);
+  const arr = []
+
+  const buscaVizinhos = (border) => {
+    console.log(border)
+    setVizinhos(border.map(item => item.borders))
+  }
+
+  //Buscando os detalhes do país -------------------------------------------------------------------------------------
+  useEffect(() => {
+    setLoading(true)
+    if (atualizaNome) {
+      axios.get(`https://restcountries.eu/rest/v2/name/${atualizaNome}`)
+        .then(res => {
+          setPaisDetalhes(res.data)
+          buscaVizinhos(res.data)
+          setLoading(false)
+        }).catch(res => {
+          setErrorMessage(error);
+          setLoading(false)
+        })
+
+    } else {
+      setLoading(false)
+      setErrorMessage(error)
+    }
+
+  }, [atualizaNome])
+
+  //Buscando os vizinhos -------------------------------------------------------------------------------------------
+  useEffect(() => {
+    setLoading(true)
+    console.log(vizinhos)
+    if (vizinhos && vizinhos[0].length !=0) {
+      vizinhos.map(item1 => item1.map(item2 => {
+        axios.get(`https://restcountries.eu/rest/v2/alpha/${item2}`)
+          .then(res => {
+
+            setLoading(false)
+            console.log(res)
+            const flag = res.data.flag
+            const getNome = res.data.name
+            arr.push({ flag, getNome })
+            if (vizinhos[0].length === arr.length) {
+              setVizinhosFinal(arr)
+            }
+          }).catch(res => {
+            setLoading(false)
+            setErrorMessage(error);
+          })
+      }
+      ))
+    } else {
+      setLoading(false)
+      setErrorMessage(error)
+    }
+  }, [vizinhos])
+
+  //Atualizando os detalhes do país escolhido ------------------------------------------------------------------------
+  const mostraPais = (item) => {
+    console.log(item)
+    setAtualizaNome(item)
+  }
+
+  console.log(errorMessage)
   return (
     <div>
       <Header />
-      Detalhes do País
+      {loading ?
+        <div className="p-5">
+          <CircularProgress />
+        </div>
+        :
+        paisesDetalhes && paisesDetalhes.map((item, index) =>
+          <div key={index} className="d-flex flex-wrap p-5">
+            <img src={item.flag} className="img-paises" />
+            <div className="d-flex flex-column p-4">
+              <div className="p-1">Nome: {item.name}</div>
+              <div className="p-1">Capital: {item.capital}</div>
+              <div className="p-1">Região: <Link to={`/${item.region}`}> {item.region} </Link></div>
+              <div className="p-1">Sub-região: {item.subregion}</div>
+              <div className="p-1">População: {item.population}</div>
+              <div className="p-1">Línguas: {item.languages.map(item => item.name)}</div>
+            </div>
+          </div>
+        )
+      }
+
+      <div className="textTitle p-5">Países Vizinhos</div>
+      <div className="d-flex flex-wrap justify-content-center">
+        {loading ?
+          <CircularProgress /> :
+          vizinhosFinal.length !=0 ? vizinhosFinal.map((item, index) => (
+            <div key={index}>
+              <button className="button-vizinhos" onClick={() => mostraPais(item.getNome)} alt={`imagem da bandeira do país ${item.getNome}`}>
+                <img src={item.flag} className="img-paises bg-light m-3" />
+              </button>
+            </div>
+          )
+          )
+          :
+          (<div className="errorMessage">{errorMessage}</div>)
+        }
+
+      </div>
     </div>
   )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,61 +3,207 @@ import { Header } from "../../Layout/Header";
 import Dropdown from "react-bootstrap/Dropdown";
 import axios from "axios";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { Link } from "react-router-dom";
 
-export const Home = () => {
+export const Home = (props) => {
+  const regiao = props.match.params.region;
   const menuInicial = ["Região", "Capital", "Língua", "País", "Código de ligação"];
+  const error = "Não foi possível buscar as informações, tente novamente..."
   const masculino = "um";
   const feminino = "uma"
   const valorDefault = "Escolha uma opção";
+  const [regiaoParam, setRegiaoParam] = useState(regiao)
   const [filtro, setFiltro] = useState("");
-  const [paises, setPaises] = useState([]);
-  const [escolhaFiltro, setEscolhaFiltro] = useState("")
+  const [menuSedundario, setMenuSecundario] = useState(null);
+  const [paises, setPaises] = useState(null);
+  const [resultadoPaises, setResultadoPaises] = useState("");
+  const [regiaoLista, setRegiaoLista] = useState(null);
+  const [capitalLista, setCapitalLista] = useState(null);
+  const [linguaLista, setLinguaLista] = useState(null);
+  const [linguaCode, setLinguaCode] = useState(null);
+  const [paisLista, setPaisLista] = useState(null);
+  const [codigoLigacao, setCodigoLigacao] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [escolhaFiltroInicial, setEscolhaFiltroInicial] = useState("");
+  const [escolhaFiltroInicialEnglish, setEscolhaFiltroInicialEnglish] = useState("");
+  const [escolhaFiltroFinal, setEscolhaFiltroFinal] = useState("");
   const [loading, setLoading] = useState(true);
 
+
+  //verificando o gênero da escolha do primeiro filtro para a correta exibição no segundo filtro ----------------
   const verificaGenero = (valorDoFiltro) => {
     if (valorDoFiltro) {
       setFiltro(valorDoFiltro)
       if (valorDoFiltro === "Região" || valorDoFiltro === "Capital" || valorDoFiltro === "Língua") {
-        setEscolhaFiltro(feminino + " " + valorDoFiltro.toLowerCase());
+        setEscolhaFiltroInicial("Escolha " + feminino + " " + valorDoFiltro.toLowerCase());
+        setEscolhaFiltroFinal("")
       } else {
-        setEscolhaFiltro(masculino + " " + valorDoFiltro.toLowerCase());
+        setEscolhaFiltroInicial("Escolha " + masculino + " " + valorDoFiltro.toLowerCase());
+        setEscolhaFiltroFinal("")
       }
     }
   }
 
-  const opcaoSelecionada = (evt) => {
+  //Obtendo a opção selecionada no primeiro filtro ----------------------------------------------------------
+  const opcaoInicialSelecionada = (evt) => {
     evt.preventDefault();
-    const busca = menuInicial.find(item => item === evt.target.value)
+    const busca = menuInicial.find(item => item === evt.target.value);
+    //Executando a verificação de gênero em conjunto com a opção selecionada
     verificaGenero(busca);
   }
 
+  //Obtendo a opção final selecionada no segundo filtro -----------------------------------------------------
+  const opcaoFinalSelecionada = (evt) => {
+    evt.preventDefault();
+    if (filtro === "Língua") {
+      let buscaLanguage = paises.map(item => item.languages)
+      let comparaLanguage = buscaLanguage.map(item => item.find(item => item.name === evt.target.value && item.iso639_1))
+      let buscaCodigo = comparaLanguage.filter(item => item != undefined)
+      setEscolhaFiltroFinal(evt.target.value);
+
+      setLinguaCode(buscaCodigo[0].iso639_1);
+    } else {
+      setEscolhaFiltroFinal(evt.target.value);
+    }
+  }
+
+  //Busca relacionada à cada item do primeiro filtro --------------------------------------------------------
+  const buscaRegioes = (regioes) => {
+    const resultado = regioes.map(item => item.region);
+    setRegiaoLista([...new Set(resultado)].sort());
+  }
+
+  const buscaCapital = (capital) => {
+    const resultado = capital.map(item => item.capital);
+    setCapitalLista([...new Set(resultado)].sort());
+  }
+
+  const buscaLinguas = (linguas) => {
+    const resultadoFinal = []
+    const resultado = linguas.map(item => item.languages.map(item => item.name));
+    for (let i = 0; i < resultado.length; i++) {
+      for (let j = 0; j < resultado[j].length; j++) {
+        resultadoFinal.push(resultado[i][j]);
+      }
+    }
+    setLinguaLista([...new Set(resultadoFinal)].sort());
+  }
+
+  const buscaPais = (pais) => {
+    const resultado = pais.map(item => item.name);
+    setPaisLista([...new Set(resultado)]);
+  }
+
+  const buscaCodigoLigacao = (codigoLigacao) => {
+    const resultadoFinal = [];
+    const resultado = codigoLigacao.map(item => item.callingCodes);
+    for (let i = 0; i < resultado.length; i++) {
+      for (let j = 0; j < resultado[j].length; j++) {
+        if (resultado[i][j]) {
+          resultadoFinal.push(parseInt(resultado[i][j]));
+        }
+      }
+    }
+    setCodigoLigacao([...new Set(resultadoFinal)].sort((a, b) => a - b))
+  }
+
+
+  //Buscando as informações iniciais de países --------------------------------------------------------------
   useEffect(() => {
+    setLoading(true)
     axios.get("https://restcountries.eu/rest/v2/all")
       .then(res => {
-        console.log(res.data)
-        setPaises(res.data)
+        setPaises(res.data);
+        setResultadoPaises(res.data);
+        buscaRegioes(res.data);
+        buscaCapital(res.data);
+        buscaLinguas(res.data);
+        buscaPais(res.data);
+        buscaCodigoLigacao(res.data);
         setLoading(false);
       }).catch(res => {
-
+        setErrorMessage(error);
+        setLoading(false);
       })
-  }, [])
+  }, []);
 
-  const verificaOpcaoEscolhida = () => {
+  //Identifica a escolha do primeiro filtro e abastece do menu secundário ----------------------------------------------------------------------------
+  const criaMenuSecundario = (escolha) => {
+    if (escolha === "Região") {
+      setEscolhaFiltroInicialEnglish("region");
+      setMenuSecundario(regiaoLista);
+    }
+    if (escolha === "Capital") {
+      setEscolhaFiltroInicialEnglish("capital");
+      setMenuSecundario(capitalLista);
+    }
+    if (escolha === "Língua") {
+      setEscolhaFiltroInicialEnglish("lang");
+      setMenuSecundario(linguaLista);
 
+    }
+    if (escolha === "País") {
+      setEscolhaFiltroInicialEnglish("name");
+      setMenuSecundario(paisLista);
+    }
+    if (escolha === "Código de ligação") {
+      setEscolhaFiltroInicialEnglish("callingcode");
+      setMenuSecundario(codigoLigacao);
+    }
   }
+
+  //Abastecimento do menu secundário sempre que um primeiro filtro é escolhido ------------------------------
+  useEffect(() => {
+    if (filtro) {
+      criaMenuSecundario(filtro);
+    }
+  }, [filtro]);
+
+
+  const pesquisaEscolha = () => {
+    setLoading(true);
+    let busca = ""
+    if (escolhaFiltroInicialEnglish === "lang") {
+      busca = linguaCode
+    } else {
+      busca = escolhaFiltroFinal
+    }
+    axios.get(`https://restcountries.eu/rest/v2/${escolhaFiltroInicialEnglish}/${busca}`)
+      .then(res => {
+        setLoading(false);
+        setResultadoPaises(res.data);
+      }).catch(res => {
+        setErrorMessage(error)
+        setLoading(false);
+      })
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    if (regiaoParam) {
+      setFiltro("Região");
+      setEscolhaFiltroFinal(regiaoParam);
+      setLoading(false)
+      if (regiaoLista) {
+        setMenuSecundario(regiaoLista);
+        setLoading(false)
+      }
+    }
+  }, [regiaoParam, regiaoLista]);
+
 
   return (
     <div>
       <Header />
+      {/*Primeiro filtro --------------------------------------------------------------------------------- */}
       <div className="d-flex justify-content-around align-items-center flex-wrap col-md-12 p-5">
-
         <div className="col-md-4 col-sm-12 col-xs-12 mb-5">
-          <div className="col-8 text-center">Filtrar por</div>
+          <div className="col-12 text-center">Filtrar por</div>
           <Dropdown className="d-flex justify-content-center">
             <Dropdown.Toggle className="dropdown col-md-8 col-sm-10 col-xs-12">{filtro || valorDefault}</Dropdown.Toggle>
             <Dropdown.Menu className="col-md-8 col-sm-10 col-xs-12">
               {menuInicial.map((item, index) => (
-                <Dropdown.Item className="dropdownItem" as="button" key={index} value={item} onClick={opcaoSelecionada}>
+                <Dropdown.Item className="dropdownItem" as="button" key={index} value={item} onClick={opcaoInicialSelecionada}>
                   {item}
                 </Dropdown.Item>
               ))}
@@ -65,15 +211,17 @@ export const Home = () => {
           </Dropdown>
         </div>
 
+        {/*Segundo filtro com base no primeiro ---------------------------------------------------------- */}
         <div className="col-md-4 col-sm-12 col-xs-12 mb-5">
-          {filtro &&
+          {
+            filtro &&
             <>
-              <div className="col-8 text-center">{filtro}</div>
+              <div className="col-12 text-center">{filtro}</div>
               <Dropdown className="d-flex justify-content-center">
-                <Dropdown.Toggle className="dropdown col-md-8 col-sm-10 col-xs-12">Escolha {escolhaFiltro}</Dropdown.Toggle>
+                <Dropdown.Toggle className="dropdown col-md-8 col-sm-10 col-xs-12">{escolhaFiltroFinal ? escolhaFiltroFinal : escolhaFiltroInicial}</Dropdown.Toggle>
                 <Dropdown.Menu className="col-md-8 col-sm-10 col-xs-12">
-                  {menuInicial.map((item, index) => (
-                    <Dropdown.Item className="dropdownItem" as="button" key={index} value={item} onClick={opcaoSelecionada}>
+                  {menuSedundario && menuSedundario.map((item, index) => (
+                    <Dropdown.Item className="dropdownItem" as="button" key={index} value={item} onClick={opcaoFinalSelecionada}>
                       {item}
                     </Dropdown.Item>
                   ))}
@@ -83,22 +231,26 @@ export const Home = () => {
           }
         </div>
 
+        {/*Botao para pesquisa com base nos filtros anteriores ------------------------------------------- */}
         <div className="col-md-4 col-sm-8 col-xs-10 d-flex justify-content-center mb-5">
-          <button className="button-pesquisar px-5 py-2">PESQUISAR</button>
+          <button className="button-pesquisar px-5 py-2" onClick={pesquisaEscolha}>PESQUISAR</button>
         </div>
 
-        {loading ?
-          <CircularProgress />
-          :
-          paises.map((item, index) => (
-            <div key={index}>
-              <img src={item.flag} alt={`Bandeira do país ${item.name}`} className="img-paises col-md-4 col-sm-12 col-xs-12 mb-5" />
-            </div>
-          ))}
+        {/*Exibição das bandeiras dos países------------------------------------------------------------- */}
+        {
+          loading ?
+            <CircularProgress />
+            :
+            resultadoPaises ? resultadoPaises.map((item, index) => (
+              <div key={index}>
+                <Link to={`/detalhes-pais/${item.name}`}><img src={item.flag} alt={`Bandeira do país ${item.name}`} className="img-paises col-md-4 col-sm-12 col-xs-12 mb-5" /></Link>
+              </div>
+            ))
+              :
+              (<div className="errorMessage">{errorMessage}</div>)
+        }
+
       </div>
     </div>
-
-
-
   )
 }
